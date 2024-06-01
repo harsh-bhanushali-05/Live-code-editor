14dfe3eee2b9d974bb3eb9acf8a7d16434a324cc
harsh-bhanushali-05 <109029538+harsh-bhanushali-05@users.noreply.github.com>
2024-06-02 00:15:29 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
 
@@ -20,6 +21,10 @@ function getAllUser(id) {
     });
 
 }
+app.use(express.static('build'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
